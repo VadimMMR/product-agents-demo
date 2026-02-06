26f5097c627e814d8c4f333e55635a0e91b071c6
VadimMMR <vadimbarsov51@gmail.com>
2026-02-06 15:37:01 +0200

    new dockerfile

--- backend/services/data_service.py
+++ backend/services/data_service.py
@@ -1,16 +1,10 @@
-from database import get_async_db
+from database import get_db_by_name
 
 async def get_all_table_data(table_name: str):
     """
-    (КОД 4) Код получения данных.
-    Безопасно читает данные из указанной таблицы.
+    (КОД 4) Переключается на нужную БД и забирает данные.
     """
-    allowed_tables = ["fruits", "vegetables", "fish", "agents"]
-    if table_name not in allowed_tables:
-        return []
-
-    async with get_async_db() as conn:
-        # Используем fetch для получения списка строк
+    # table_name здесь выступает и как имя БД
+    async with get_db_by_name(table_name) as conn:
         rows = await conn.fetch(f"SELECT * FROM {table_name} ORDER BY id DESC LIMIT 100")
-        # Конвертируем записи в обычные словари для JSON
         return [dict(row) for row in rows]
